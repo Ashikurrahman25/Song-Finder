81df3784c1a99bfd8ac27b4c771d3be1ff03898f
Ashikur Rahman <62755604+Ashikurrahman25@users.noreply.github.com>
2021-02-08 23:00:10 +0600

    Modifications

--- app.js
+++ app.js
@@ -10,14 +10,21 @@ const searchForSongs = async() => {
 
 const showSongs = songs => {
     const songContainer = document.getElementById('song-container');
+    const lyricContainer = document.getElementById('lyric-container');
     songContainer.innerHTML ='';
+    lyricContainer.innerText = '';
+
     songs.forEach(song => {
 
         const songDiv = document.createElement('Div');
         songDiv.className = "single-result row align-items-center my-3 p-3";
 
         songDiv.innerHTML = `
-        <div class="col-md-9">
+
+        <div id="thumbnail" class="col-md-3"">
+            <img style="width: 100%; border-radius: 10px;" src="${song.album.cover}" alt="">
+        </div>
+        <div class="col-md-6">
             <h3 class="lyrics-name">${song.title}</h3>
             <p class="author lead">Album by <span>${song.artist.name}t</span></p>
             <audio controls>
@@ -25,7 +32,7 @@ const showSongs = songs => {
             </audio>
         </div>
         <div class="col-md-3 text-md-right text-center">
-            <button onclick ="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-success">Get Lyrics</button>
+            <button onclick ="getLyric('${song.artist.name}', '${song.title}')" class="btn btn-primary">Get Lyrics</button>
         </div>`;
 
         songContainer.appendChild(songDiv);
@@ -44,6 +51,5 @@ const showLyrics = lyrics =>{
     const lyricContainer = document.getElementById('lyric-container');
     lyricContainer.innerText = lyrics;
     document.documentElement.scrollTop = 0;
-    console.log(lyrics);
 }
 
